b8cbf8d3afb6c66eb66e8f33c4b13ddaec72721c
Julio César <zzantares@gmail.com>
2018-04-15 01:17:43 -0500

    Update settings

--- init.el
+++ init.el
@@ -10,8 +10,9 @@
 ;; TODO: Adopt the usage of use-package's :hook keyword
 ;; TODO: Conflicting style magit-diff and highlight-chars tab: https://emacs.stackexchange.com/q/38695/12340
 ;; TODO: Explore the org-mode features from https://github.com/dieggsy/dotfiles/tree/master/emacs.d
-;; TODO: Org easy templates expansion not working
-;; TODO: Bubble lines not working, tested on haskell
+;; TODO: Org easy templates expansion not working (https://github.com/yjwen/org-reveal/issues/323)
+;; TODO: Explore intero as replacement to haskell-mode
+;; TODO: Add settings for disabling auto-save on .gpg files (https://www.reddit.com/r/emacs/comments/46lv2q/is_there_any_easy_way_to_make_org_files_password/d08j4fb/)
 
 ;; ===================================================
 ;; NOTES & REMINDERS
@@ -255,6 +256,83 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   (evil-goto-mark ?[)
   (evil-visual-char)
   (evil-goto-mark ?]))
+
+;; See: https://emacs.stackexchange.com/a/10233/12340
+(defun zz-lisp-indent-function (indent-point state)
+  "This function is the normal value of the variable `lisp-indent-function'.
+The function `calculate-lisp-indent' calls this to determine
+if the arguments of a Lisp function call should be indented specially.
+
+INDENT-POINT is the position at which the line being indented begins.
+Point is located at the point to indent under (for default indentation);
+STATE is the `parse-partial-sexp' state for that position.
+
+If the current line is in a call to a Lisp function that has a non-nil
+property `lisp-indent-function' (or the deprecated `lisp-indent-hook'),
+it specifies how to indent.  The property value can be:
+
+* `defun', meaning indent `defun'-style
+  \(this is also the case if there is no property and the function
+  has a name that begins with \"def\", and three or more arguments);
+
+* an integer N, meaning indent the first N arguments specially
+  (like ordinary function arguments), and then indent any further
+  arguments like a body;
+
+* a function to call that returns the indentation (or nil).
+  `lisp-indent-function' calls this function with the same two arguments
+  that it itself received.
+
+This function returns either the indentation to use, or nil if the
+Lisp function does not specify a special indentation."
+  (let ((normal-indent (current-column))
+        (orig-point (point)))
+    (goto-char (1+ (elt state 1)))
+    (parse-partial-sexp (point) calculate-lisp-indent-last-sexp 0 t)
+    (cond
+     ;; car of form doesn't seem to be a symbol, or is a keyword
+     ((and (elt state 2)
+           (or (not (looking-at "\\sw\\|\\s_"))
+               (looking-at ":")))
+      (if (not (> (save-excursion (forward-line 1) (point))
+                  calculate-lisp-indent-last-sexp))
+          (progn (goto-char calculate-lisp-indent-last-sexp)
+                 (beginning-of-line)
+                 (parse-partial-sexp (point)
+                                     calculate-lisp-indent-last-sexp 0 t)))
+      ;; Indent under the list or under the first sexp on the same
+      ;; line as calculate-lisp-indent-last-sexp.  Note that first
+      ;; thing on that line has to be complete sexp since we are
+      ;; inside the innermost containing sexp.
+      (backward-prefix-chars)
+      (current-column))
+     ((and (save-excursion
+             (goto-char indent-point)
+             (skip-syntax-forward " ")
+             (not (looking-at ":")))
+           (save-excursion
+             (goto-char orig-point)
+             (looking-at ":")))
+      (save-excursion
+        (goto-char (+ 2 (elt state 1)))
+        (current-column)))
+     (t
+      (let ((function (buffer-substring (point)
+                                        (progn (forward-sexp 1) (point))))
+            method)
+        (setq method (or (function-get (intern-soft function)
+                                       'lisp-indent-function)
+                         (get (intern-soft function) 'lisp-indent-hook)))
+        (cond ((or (eq method 'defun)
+                   (and (null method)
+                        (> (length function) 3)
+                        (string-match "\\`def" function)))
+               (lisp-indent-defform state indent-point))
+              ((integerp method)
+               (lisp-indent-specform method state
+                                     indent-point normal-indent))
+              (method
+               (funcall method indent-point state))))))))
 
 (defun flyspell-goto-previous-error (arg)
   "Go to ARG previous spelling error."
@@ -520,6 +598,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   (add-hook 'git-commit-setup-hook #'git-commit-turn-on-flyspell)
   :config
   (flyspell-mode 1)
+  (setq ispell-program-name "aspell")
   :general
   (:keymaps 'normal
             "]s" 'flyspell-goto-next-error
@@ -897,6 +976,13 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
            :keymaps '(html-mode-map web-mode-map css-mode-map nxml-mode-map)
             "," 'emmet-expand-line))
 
+(use-package drag-stuff
+  :init
+  (drag-stuff-global-mode 1)
+  :config
+  (add-to-list 'drag-stuff-except-modes 'org-mode)
+  (drag-stuff-define-keys))
+
 (use-package help-mode
   :straight nil
   :init
@@ -1007,6 +1093,11 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 (use-package octave
   :straight nil
   :mode ("\\.m$" . octave-mode))
+
+(use-package lisp-mode
+  :straight nil
+  :hook (emacs-lisp-mode . (lambda ()
+                             (setq-local lisp-indent-function #'zz-lisp-indent-function))))
 
 (use-package elm-mode
   :init
@@ -1231,8 +1322,8 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 ;; (set-face-attribute 'default nil :height 160 :family "Operator Mono")
 ;; (set-face-attribute 'default nil :height 150 :family "Fira Code")
 ;; (set-face-attribute 'default nil :height 150 :family "Hack")
-;; (set-face-attribute 'default nil :height 150 :family "Monaco")
-(set-face-attribute 'default nil :height 150 :family "Menlo")
+(set-face-attribute 'default nil :height 150 :family "Monaco")
+;; (set-face-attribute 'default nil :height 150 :family "Menlo")
 ;; (set-face-attribute 'default nil :height 160 :family "Roboto Mono")
 ;; (set-face-attribute 'default nil :height 170 :family "Fantasque Sans Mono")
 ;; (set-face-attribute 'default nil :height 160 :family "Fira Mono")
