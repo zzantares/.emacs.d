2fa7380ac58e557777b9b23975c3b73af71a23da
Julio César <zzantares@gmail.com>
2019-12-09 14:05:17 -0600

    Improve support for Scala

--- init.el
+++ init.el
@@ -1474,6 +1474,16 @@ Lisp function does not specify a special indentation."
   :interpreter ("scala" . scala-mode)
   :hook (scala-mode . (lambda () (evil-smartparens-mode -1))))
 
+(use-package sbt-mode
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map))
+
 (use-package clojure-mode)
 
 (use-package cider)
